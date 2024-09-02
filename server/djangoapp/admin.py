d75472c18701d80002237de667096bd323bbc3a0
SanK024 <ksanchit024@gmail.com>
2024-09-02 14:46:50 -0400

    modlue 4 changes

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,18 +1,18 @@
-# from django.contrib import admin
-# from .models import related models
 from django.contrib import admin
 from .models import CarMake, CarModel
 
-# Registering models with their respective admins
-admin.site.register(CarMake)
-admin.site.register(CarModel)
+class CarModelInline(admin.TabularInline):
+    model = CarModel
+    extra = 1
 
-# Register your models here.
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'car_make', 'type', 'year')
+    search_fields = ('name', 'car_make__name')
 
-# CarModelInline class
+class CarMakeAdmin(admin.ModelAdmin):
+    inlines = [CarModelInline]
+    list_display = ('name', 'description')
+    search_fields = ('name',)
 
-# CarModelAdmin class
-
-# CarMakeAdmin class with CarModelInline
-
-# Register models here
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)

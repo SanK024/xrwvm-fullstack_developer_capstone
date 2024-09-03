14cff33a9431882cde994fed676215aa8afca429
Sanchit <106865303+SanK024@users.noreply.github.com>
2024-09-03 15:02:44 +0530

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -10,16 +10,16 @@ class CarMake(models.Model):
     description = models.TextField()
     
     def __str__(self):
-        return self.name 
+        return self.name
 
 
 class CarModel(models.Model):
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     CAR_TYPES = [
-        ('SEDAN', 'Sedan'), 
-        ('SUV', 'SUV'), 
-        ('WAGON', 'Wagon'), 
+        ('SEDAN', 'Sedan'),
+        ('SUV', 'SUV'),
+        ('WAGON', 'Wagon'),
         # Add more choices as required
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
@@ -30,4 +30,4 @@ class CarModel(models.Model):
                                ])
     
     def __str__(self):
-        return self.name  
+        return self.name

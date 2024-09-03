f56384450862d0c2004f0584957be65de329299b
Sanchit <106865303+SanK024@users.noreply.github.com>
2024-09-03 16:30:09 +0530

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -110,6 +110,8 @@ def add_review(request):
     if (request.user.is_anonymous is False):
         data = json.loads(request.body)
         try:
+            response = post_review(data)
+            print(response)
             return JsonResponse({"status": 200})
         except Exception as err:
             return JsonResponse(
